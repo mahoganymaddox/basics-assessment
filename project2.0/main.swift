8a22685601cc4b06d02a0c945336201fb5da2ba1
Mahogany Maddox <mahoganymaddox@interapts-macbook-pro-76.local>
2018-01-19 13:51:02 -0500

    Initial Commit

---
+++ project2.0/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  project2.0
+//
+//  Created by Mahogany Maddox on 1/19/18.
+//  Copyright © 2018 Mahogany Maddox. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
